60d7ba5c2a970089f9c8d569738e6e5501b42e07
PixelGuff <pixelguff@gmail.com>
2018-07-13 16:19:26 +0100

    Added disambigulator

--- wiki.py
+++ wiki.py
@@ -1,7 +1,13 @@
 import sopel
-import wikipedia as wk
+import wikipedia as wik
 
-@sopel.module.commands('wiki')
-def wiki(bot, trigger):
-        result = wk.summary(trigger, sentences=1)
+@sopel.module.commands('wk')
+def wk(bot, trigger):
+	arg_list = trigger.split(" ", 1)
+	try:
+		result = wik.summary(str(arg_list[1]), sentences=1)
+	except wik.exceptions.DisambiguationError as e:
+		result = "DISAMBIGULATOR: Did you mean one of these?"
+	        for option in e.options:
+        	        result = result+" '"+str(option)+"'"
 	bot.say(result)
