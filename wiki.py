aaa293ea946245f1c4367ce7dbf1876bac60ee9d
PixelGuff <pixelguff@gmail.com>
2018-07-20 14:05:31 +0100

    Google search, and tweaks

--- wiki.py
+++ wiki.py
@@ -6,7 +6,8 @@ def wk(bot, trigger):
 	arg_list = trigger.split(" ", 1)
 	try:
 		if arg_list[1].lower() == "random":
-			result = wik.summary(wik.random(), sentences=1)
+			wrandom = wik.random()
+			result = "Random article - '" + wrandom + "': " + wik.summary(wrandom, sentences=1)
 		else:
 			result = wik.summary(str(arg_list[1]), sentences=1)
 
